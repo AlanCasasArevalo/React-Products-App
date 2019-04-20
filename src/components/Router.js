3592a38a0df60be8ea6d28b6a57ec83a072a7532
Alan Casas <alancasasarevalo@gmail.com>
2019-04-20 09:30:15 +0200

    [FEATURE][ERROR PAGE AND US] - Error page was added - Router was modified - Us component was added

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import Init from "./Init";
+import Us from "./Us";
+import ErrorPage from "./ErrorPage";
 
 class Router extends Component{
     render() {
@@ -8,6 +10,8 @@ class Router extends Component{
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Init}/>
+                    <Route exact path="/us" component={Us}/>
+                    <Route component={ErrorPage}/>
                 </Switch>
             </BrowserRouter>
         );
---
+++ src/components/Us.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Us extends Component{
+    render() {
+        return (
+            <div>
+                <h1>sobre nosotros</h1>
+            </div>
+        );
+    }
+}
+
+export default Us;

1157105146516177de61b7e658d737feea8379dc
Alan Casas <alancasasarevalo@gmail.com>
2019-04-20 10:37:16 +0200

    [FEATURE][DETAIL PRODUCT] - Product detail was finished with their own css style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <img src="./img/logo.png" alt="Logo de la aplicacion"/>
+            <img src="/img/logo.png" alt="Logo de la aplicacion"/>
         </header>
     )
 };
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react'
-import Products from "./Products";
 import PropTypes from "prop-types";
+import ProductDetailStyle from './ProductDetailStyle.css'
 
 class ProductDetail extends Component{
     render() {
         const {id, nombre, precio, imagen, descripcion} = this.props.productDetail;
+
+        if (!id) return null;
+
         return (
-            <div>
-                <h1>Product detail</h1>
+            <div className="info-producto">
+                <div className="imagen">
+                    <img src={`/img/${imagen}.png`} alt="camiseta"/>
+                </div>
+                <div className="info">
+                    <h2>{nombre}</h2>
+                    <p className="precio">$ {precio}</p>
+                    <p>{descripcion}</p>
+                </div>
             </div>
         );
     }

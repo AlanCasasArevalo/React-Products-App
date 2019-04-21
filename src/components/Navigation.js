8d1fb69da28076291c5db32423254639e7d96c99
Alan Casas <alancasasarevalo@gmail.com>
2019-04-21 09:21:18 +0200

    [FEATURE][LINK -> NAVLINK] - Into navigation bar we exchanged link into nav link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import NavegacionStyle from './NavegacionStyle.css'
 
 const Navigation = () => {
     return (
         <nav className="navegacion">
-            <Link to={'/us'}>Nosotros</Link>
-            <Link to={'/products'}>Productos</Link>
-            <Link to={'/contact'}>Contacto</Link>
+            <NavLink to={'/us'} activeClassName="activo">Nosotros</NavLink>
+            <NavLink to={'/products'} activeClassName="activo">Productos</NavLink>
+            <NavLink to={'/contact'} activeClassName="activo">Contacto</NavLink>
         </nav>
     )
 };
